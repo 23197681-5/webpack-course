f274a0ca667ceb295b8d43e886af57652b15b3f7
willianlauber <lcwillian_hd@hotmail.com>
2017-01-14 15:30:25 -0200

    index.js importing

--- app/index.js
+++ app/index.js
@@ -1,15 +1,2 @@
 // let name = "Willian";
 // console.log(name);
-
-import * as Math from './modules/multipleNamedExports/math'
-//import {cube, pi as pai}
-console.log(pi);
-console.log(api("Ana"));
-
-var person = new Person();
-person.toString();
-//console.log();
-
-console.log("---------------");
-console.log(Math.cube(53));
-console.log(Math.pi);
--- app/modules/singleDefautExports/index.js
+++ app/modules/singleDefautExports/index.js
@@ -2,3 +2,12 @@ import pi from './pi'
 import api from './api'
 import Person from './person'
 import {log, logTittle} from '../WindowLogger'
+
+export default function(){
+  logTittle('SingleDefaultsExports');
+  log(pi);
+  log(api("Ana"));
+  var person = new Person();
+  person.toString();
+  //console.log();
+}
