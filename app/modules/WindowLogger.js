289bd00c5edd7e35f45529fc5e2108735dd142e0
willianlauber <lcwillian_hd@hotmail.com>
2017-01-14 14:40:30 -0200

    Added index.js in every folder

--- app/modules/WindowLogger.js
+++ app/modules/WindowLogger.js
@@ -2,6 +2,6 @@ import $ from 'jquery';
 export function log(content){
   $('#content').append("<h4 class='bg-info'>"+ content + "</h4>"
 }
-export function log(content){
+export function logTittle(content){
   $('#content').append("<h4 class='bg-primary'>"+ content + "</h4>"
 }
--- app/modules/singleDefautExports/index.js
+++ app/modules/singleDefautExports/index.js
@@ -1,3 +1,4 @@
 import pi from './pi'
 import api from './api'
 import Person from './person'
+import {log, logTittle} from '../WindowLogger'
