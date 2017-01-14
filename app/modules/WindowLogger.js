dbab65b65e9d57cd9df88cdd74bde7b65d693424
willianlauber <lcwillian_hd@hotmail.com>
2017-01-14 14:34:50 -0200

    Added jquery bootstrap editing

---
+++ app/modules/WindowLogger.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+export function log(content){
+  $('#content').append("<h4 class='bg-info'>"+ content + "</h4>"
+}
+export function log(content){
+  $('#content').append("<h4 class='bg-primary'>"+ content + "</h4>"
+}
