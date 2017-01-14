f9d61bcce859a828dee46461e7b62a27382ba01c
willianlauber <lcwillian_hd@hotmail.com>
2017-01-14 15:54:03 -0200

    index.js importing by functions

--- app/modules/WindowLogger.js
+++ app/modules/WindowLogger.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 export function log(content){
-  $('#content').append("<h4 class='bg-info'>"+ content + "</h4>"
+  $('#content').append("<h4 class='bg-info'>"+ content + "</h4>")
 }
 export function logTittle(content){
-  $('#content').append("<h4 class='bg-primary'>"+ content + "</h4>"
+  $('#content').append("<h4 class= 'bg-primary'>"+ content + "</h4>")
 }
